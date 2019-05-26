61d7ed89957edd6137ab8e5b59d09a1abe8303d4
starkend <danstarken@gmail.com>
2019-05-26 08:47:25 -0500

    Fix AWS base url

--- src/main/webapp/src/environments/environment.prod.ts
+++ src/main/webapp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://cedatabase.cv9kgtnylp6n.us-east-2.rds.amazonaws.com'
+  apiUrl: 'http://ec2-18-222-213-190.us-east-2.compute.amazonaws.com:8080'
 };
