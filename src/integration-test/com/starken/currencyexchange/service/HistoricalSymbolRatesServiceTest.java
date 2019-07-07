91599d1868cd2920f257e70ea8192edb966d0306
starkend <danstarken@gmail.com>
2019-07-07 12:10:55 -0500

    Test populate Elasticsearch index with Ecb Service historical data.

--- src/integration-test/com/starken/currencyexchange/service/HistoricalSymbolRatesServiceTest.java
+++ src/integration-test/com/starken/currencyexchange/service/HistoricalSymbolRatesServiceTest.java
@@ -1,6 +1,7 @@
 package com.starken.currencyexchange.service;
 
 import com.starken.currencyexchange.CurrencyexchangeApplication;
+import com.starken.currencyexchange.dto.HistoricalSymbolRatesDto;
 import com.starken.currencyexchange.model.HistoricalRates;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,10 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = CurrencyexchangeApplication.class)
@@ -27,26 +30,26 @@ public class HistoricalSymbolRatesServiceTest {
     @Autowired
     ElasticsearchTemplate elasticsearchTemplate;
 
-//    @Test
-//    public void whenHistoricalRatesByBase_thenReturnPopulatedHistoricalRatesSymbolDto() {
-//        HistoricalSymbolRatesDto historicalSymbolRatesDto;
-//
-//        historicalSymbolRatesDto = ecbService.getHistoricalRatesByBase("USD");
-//
-//        assertNotNull(historicalSymbolRatesDto);
-//        assertFalse(historicalSymbolRatesDto.getRates().isEmpty());
-//
-//        Date date = new Date();
-//
-//        HistoricalRates newRates = new HistoricalRates(date.toString(),
-//                historicalSymbolRatesDto.getBase(),
-//                historicalSymbolRatesDto.getRates());
-//
-//        HistoricalRates newDto = historicalSymbolRatesService.save(newRates);
-//
-//        assertNotNull(newDto);
-//
-//    }
+    @Test
+    public void whenHistoricalRatesByBase_thenReturnPopulatedHistoricalRatesSymbolDto() {
+        HistoricalSymbolRatesDto historicalSymbolRatesDto;
+
+        historicalSymbolRatesDto = ecbService.getHistoricalRatesByBase("AUD");
+
+        assertNotNull(historicalSymbolRatesDto);
+        assertFalse(historicalSymbolRatesDto.getRates().isEmpty());
+
+        Date date = new Date();
+
+        HistoricalRates newRates = new HistoricalRates(date.toString(),
+                historicalSymbolRatesDto.getBase(),
+                historicalSymbolRatesDto.getRates());
+
+        HistoricalRates newDto = historicalSymbolRatesService.save(newRates);
+
+        assertNotNull(newDto);
+
+    }
 
     @Test
     public void whenFindAll_thenReturnAll() {
